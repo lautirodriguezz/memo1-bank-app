267a4939db4aa2c6a62beeb591df388253ebcc09
lautirodriguezz <66536114+lautirodriguezz@users.noreply.github.com>
2023-11-09 22:41:00 -0300

    Add files via upload

---
+++ memo1-bank-app-master(ENTREGA)/src/main/java/com/aninfo/Memo1BankApp.java
@@ -0,0 +1,101 @@
+package com.aninfo;
+
+import com.aninfo.model.Account;
+import com.aninfo.service.AccountService;
+import com.aninfo.model.Transaction;
+import com.aninfo.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Optional;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@SpringBootApplication
+@EnableSwagger2
+public class Memo1BankApp {
+
+	@Autowired
+	private AccountService accountService;
+	@Autowired
+	private TransactionService transactionService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Memo1BankApp.class, args);
+	}
+
+	@PostMapping("/accounts")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Account createAccount(@RequestBody Account account) {
+		return accountService.createAccount(account);
+	}
+
+	@GetMapping("/accounts")
+	public Collection<Account> getAccounts() {
+		return accountService.getAccounts();
+	}
+
+	@GetMapping("/accounts/{cbu}")
+	public ResponseEntity<Account> getAccount(@PathVariable Long cbu) {
+		Optional<Account> accountOptional = accountService.findById(cbu);
+		return ResponseEntity.of(accountOptional);
+	}
+
+	@PutMapping("/accounts/{cbu}")
+	public ResponseEntity<Account> updateAccount(@RequestBody Account account, @PathVariable Long cbu) {
+		Optional<Account> accountOptional = accountService.findById(cbu);
+
+		if (!accountOptional.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		account.setCbu(cbu);
+		accountService.save(account);
+		return ResponseEntity.ok().build();
+	}
+
+	@DeleteMapping("/accounts/{cbu}")
+	public void deleteAccount(@PathVariable Long cbu) {
+		accountService.deleteById(cbu);
+	}
+
+	@PostMapping("accounts/{cbu}/transaction")
+	public Transaction createTransaction(@PathVariable Long cbu, @RequestParam Double sum, @RequestParam String typeTransaction) { 
+		return transactionService.createTransaction(cbu, sum, typeTransaction); 
+	}
+
+	@DeleteMapping("/transactions/{id}")
+	public void deleteTransaction(@PathVariable Long id) {
+		transactionService.deleteTransaction(id);
+	}
+
+	@GetMapping("/accounts/{cbu}/transactions")
+	public Collection<Transaction> getTransactions(@PathVariable Long cbu) {
+		return transactionService.getTransactionsByCBU(cbu);
+	}
+
+	@GetMapping("/transactions/{id}")
+	public ResponseEntity<Transaction> getTransaction(@PathVariable Long id) {
+		Optional<Transaction> transactionOptional = transactionService.getTransactionById(id);
+		return ResponseEntity.of(transactionOptional);
+	}
+
+	@Bean
+	public Docket apiDocket() {
+		return new Docket(DocumentationType.SWAGGER_2)
+			.select()
+			.apis(RequestHandlerSelectors.any())
+			.paths(PathSelectors.any())
+			.build();
+	}
+}
